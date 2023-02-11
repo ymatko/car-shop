77a0bd9626bcce97c0b3db2639392837ff361494
Yevhen Matko <n246v1j80x6t@gmail.com>
2023-02-11 19:53:08 +0100

    Home page and redirect

---
+++ CarShop/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using CarShop.Data.Interfaces;
+using CarShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarShop.Controllers
+{
+	public class HomeController : Controller
+	{
+        private readonly IAllCars _carRep;
+
+        public HomeController(IAllCars carRep)
+        {
+            _carRep = carRep;
+        }
+
+        public  ViewResult Index()
+        {
+            var homeCars = new HomeViewModel
+            {
+                favCars = _carRep.getFavCars
+            };
+            return View(homeCars);
+        }
+    }
+}
---
+++ CarShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CarShop.Data.Models;
+
+namespace CarShop.ViewModels
+{
+	public class HomeViewModel
+	{
+		public IEnumerable<Car> favCars { get; set; }
+	}
+}
